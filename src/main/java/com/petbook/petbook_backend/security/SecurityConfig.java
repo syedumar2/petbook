e8f69fbbd4ac71fb14e8eb4098000f124257bdd7
syedumar2 <to.syedumar@gmail.com>
2025-09-24 01:08:45 +0530

    CORS fix

--- src/main/java/com/petbook/petbook_backend/security/SecurityConfig.java
+++ src/main/java/com/petbook/petbook_backend/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://syedumar2.github.io/petbook-frontend"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
