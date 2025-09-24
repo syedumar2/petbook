d7622fff2d18297c9c353b035862edb4f31d26ff
syedumar2 <to.syedumar@gmail.com>
2025-09-24 19:21:39 +0530

    CORS configuration: switching from gh pages to render

--- src/main/java/com/petbook/petbook_backend/security/SecurityConfig.java
+++ src/main/java/com/petbook/petbook_backend/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://syedumar2.github.io","http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://petbook-c8nv.onrender.com","http://localhost:3000"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
